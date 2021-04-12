32e5594b86baf883bf79a5ad1d07b7916d54d6de
coudert <paulinecoudert@gmail.com>
2021-04-12 11:52:13 +0200

    cocher / decocher mon boutton Ajout/suppression dans cart

--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 import { Article } from '../_models/article';
 
 @Component({
@@ -12,7 +13,9 @@ export class CartPage implements OnInit {
 
   items: Article[]; // lier à la liste
 
-  constructor() { }
+  constructor(
+    private actionSheetCtrl: ActionSheetController
+  ) { }
 
   ngOnInit() {
     this.items = []; //initialiser la liste au depart qu'elle soit nulle
@@ -24,4 +27,22 @@ export class CartPage implements OnInit {
     this.newItem= null; // je réinititalise ma liste vide
     }
   }
+
+
+
+    async openActionSheet(item: Article){ //methode qui prend en parametre l'element sur lequel je click l'article
+      const as = await this.actionSheetCtrl.create({ //le as = await nouvelle syntaxte du .then
+        header: '',
+        buttons: [
+          { text: item.isChecked ? 'Décocher' : 'Cocher', handler: () => {item.isChecked= !item.isChecked;}}, //mettre l'inverse pour cocher décocher
+          { text: 'Supprimer', handler: () =>{ //supression de l'article dans la liste
+            const i = this.items.indexOf(item);
+            this.items.splice(i, 1);
+        }}
+      ]
+      });
+       as.present();
+       
+  }
+  
 }
